aff2adf6b67a8016805e80543841e53cd0eef172
rahulkumar161098 <rahulkumar161098@gmail.com>
2022-05-05 20:15:43 +0530

    add css

--- todos/settings.py
+++ todos/settings.py
@@ -119,8 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIR=[
-    BASE_DIR /'STATIC'
+STATICFILES_DIRS=[
+     'static'
 ]
 
 
